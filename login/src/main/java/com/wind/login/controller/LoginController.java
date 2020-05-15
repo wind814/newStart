009247263a9a15895d6700687c9828a945ba9c95
wind814 <wanghainanwing@126.com>
2020-05-15 08:09:55 +0800

    加入redis

--- login/src/main/java/com/wind/login/controller/LoginController.java
+++ login/src/main/java/com/wind/login/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.wind.login.controller;
 import com.wind.login.dao.AccountMapper;
 import com.wind.login.entity.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,9 @@ public class LoginController {
         @Autowired
         private AccountMapper accountMapper;
 
+        @Autowired
+        private StringRedisTemplate redisTemplate;
+
         @RequestMapping(value = "login" ,method = RequestMethod.GET)
         public String login(@PathVariable("account") String account){
 
@@ -25,6 +29,11 @@ public class LoginController {
 
             return "token";
 
+        }
+
+        @RequestMapping(value = "getAccount" ,method = RequestMethod.GET)
+        public String getAccount(@PathVariable("account") String account){
+            return redisTemplate.opsForValue().get("name");
         }
 
 }
