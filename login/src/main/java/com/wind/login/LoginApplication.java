9be4b196066f8441da85975f8fd62229e841238c
wind814 <wanghainanwing@126.com>
2020-06-25 17:04:14 +0800

    1、加入eureka server、login eureka client 。 2、修改了spring boot版本为2.2.3,以对应spring cloud 版本Hoxton.SR5、eureka版本2.2.3.  需要注意 eureka的artifactId 为 spring-cloud-starter-netflix-eureka-server 3、后面应该根据 https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-dependencies  不同版本号对应不同版本spring boot等工具。防止版本冲突造成的异常情况。 4、当出现启动一次,或者其他异常的情况,可以根据异常 例如 java.lang.ArrayStoreException 异常,新增 DEBUG 断点 java Exception point ,进行跟踪。

--- login/src/main/java/com/wind/login/LoginApplication.java
+++ login/src/main/java/com/wind/login/LoginApplication.java
@@ -3,10 +3,12 @@ package com.wind.login;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@MapperScan("com.wind.login.dao")
 @tk.mybatis.spring.annotation.MapperScan("com.wind.login.dao")
+@EnableEurekaClient
 public class LoginApplication {
 
     public static void main(String[] args) {
